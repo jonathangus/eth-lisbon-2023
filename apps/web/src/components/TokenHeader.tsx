e8e2a628cd64cd78df8777d18c7862fd6f4e3bfa
Jonathan <jonathangus@proton.me>
2023-05-13 15:25:13 +0100

    stats

--- apps/web/src/components/TokenHeader.tsx
+++ apps/web/src/components/TokenHeader.tsx
@@ -1,12 +1,14 @@
 import { TBAToken } from 'shared-config';
 import TokenDisplay from './TokenDisplay';
 import FollowProfile from './actions/FollowProfile';
+import { useProfileStatsQuery } from '../hooks/useProfileStatsQuery';
 
 type Props = {
   token: TBAToken;
 };
 
 const TokenHeader = ({ token }: Props) => {
+  const stats = useProfileStatsQuery(token.profileId);
   return (
     <div className="p-12 bg-slate-300">
       <div className="flex">
@@ -16,6 +18,16 @@ const TokenHeader = ({ token }: Props) => {
         <div className="ml-2">
           <div>{token.accountAddress}</div>
           <div>{token.handle}.lens</div>
+        </div>
+        <div className="ml-5">
+          {stats.data && (
+            <div>
+              <div>followers:{stats.data.totalFollowers}</div>
+              <div>collects:{stats.data.totalCollects}</div>
+              <div>posts:{stats.data.totalPosts}</div>
+              <div>totalFollowing: {stats.data.totalFollowing}</div>
+            </div>
+          )}
         </div>
         <div>
           <FollowProfile token={token} />
